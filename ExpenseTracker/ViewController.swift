35e9f58d6568ca4408c45d1f094314fe541800e4
Michael Lyapin <lyapin.dev@gmail.com>
2023-06-28 13:47:50 +0300

    Initial Commit

---
+++ ExpenseTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpenseTracker
+//
+//  Created by Ляпин Михаил on 28.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
